a8937175485fc0f5c3dca844481b3611b6abf9c7
Rozaan <55173397+rozaanmuharif01@users.noreply.github.com>
2021-07-06 07:37:52 +0700

    Add files via upload

---
+++ db_user.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 28 Jun 2021 pada 07.40
+-- Versi server: 10.4.17-MariaDB
+-- Versi PHP: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `db_user`
+--
+
+CREATE TABLE `db_user` (
+  `username` varchar(254) NOT NULL,
+  `password` varchar(254) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
